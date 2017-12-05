273e8e27b0aeb8f680ebe96cd2f66a51f91ef500
ejackson682 <33526853+ejackson682@users.noreply.github.com>
2017-12-05 19:11:59 +0000

    Add files via upload

---
+++ ZombiesElif.py
@@ -0,0 +1,10 @@
+zombies = int(input("Enter the number of zombies: "))
+if zombies > 20:
+    print("Ahhh! Zombies!")
+elif zombies > 10:
+    print("There are just half a Minecraft zombie apocalypse.")
+elif zombies == 0:
+    print("No zombies here! Phew!")
+else:
+    print("You know, zombies aren't so bad.")
+    
